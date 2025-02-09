0bc18de774be32d0df56193950e22d880cd8700a
Dinesh Borude <borudedinesh2@gmail.com>
2025-02-10 00:42:11 +0530

    dropdown donw

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ playwright interview questions :
 
 1) locators in playwright ?
 2) Playwright vs Selenium ?
-3) Why Playwright?
+3) Why Playwright ?
 4) Open browser without incognito in playwright? --> chromium.launchPersistentContext( ", {headless:false, channel: 'chrome'})
 5) How to handle browser context in playwright?
-6) 
+6) What is async await ?
+7) What is arrow function ? 
+8) 
--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -1,26 +1,22 @@
-import test, { Browser, chromium, Locator, Page } from "playwright/test";
+import { test, expect, Locator } from "playwright/test";
 
-test('dropdown handling', async() => {
-
-   const browser:Browser = await chromium.launch({headless:false});
-   const page:Page = await browser.newPage(); 
-
+test('dropdown handling', async ({ page }) => {
    const url = "https://www.saucedemo.com/";
 
    const username = "standard_user";
    const password = "secret_sauce";
 
-   const usernameField:Locator = page.locator("#user-name");
-   const passwordField:Locator = page.locator('id = password');
-   
+   const usernameField: Locator = page.locator("#user-name");
+   const passwordField: Locator = page.locator("#password"); 
+   const loginButton = page.getByTestId("login-button"); 
+   const dropdownField = 'select.product_sort_container';
 
    await page.goto(url);
-
    await usernameField.fill(username);
    await passwordField.fill(password);
-
+   await loginButton.click();
+   await page.selectOption(dropdownField, {label : 'Price (high to low)'});
 
 
    await page.waitForTimeout(5000);
-
 });
