b7cb63cbdd1e01f2e4d6f99a08812f90bd293e58
Dinesh Borude <borudedinesh2@gmail.com>
2025-02-14 23:59:10 +0530

    press sequentially

---
+++ tests/press_sequentially.spec.ts
@@ -0,0 +1,12 @@
+import test, { Locator } from "playwright/test";
+
+test('type slowly test', async ({page}) => {
+    const url = "https://www.flipkart.com/";
+    const searchbox:Locator = page.getByRole('textbox', { name: 'Search for Products, Brands' });
+
+    await page.goto(url,{waitUntil:'load'});
+    await searchbox.pressSequentially('iphone',{delay:500});
+
+
+
+})
