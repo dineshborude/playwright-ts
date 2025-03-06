cebae050704ac242b0f314095a17018cab8fbacb
Dinesh Borude <borudedinesh2@gmail.com>
2025-03-06 23:46:04 +0530

    first js

---
+++ samplecodes/JS_Learnings/js_working.js
@@ -0,0 +1,15 @@
+
+var n = 2;
+var ans;
+
+function square(n) {
+    ans =  n * n;
+    return ans;
+}
+
+ square2 = square(n);
+ square4 = square(4);
+
+console.log(square2);
+console.log(square4);
+
