21d30301f301afb0f67551ca0c2f21ea3d7ebd41
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-25 01:58:04 +0530

    playwright

---
+++ src/tests/login.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect, Browser, Page, Locator} from '@playwright/test';
+import {webkit, chromium, firefox} from 'playwright';
+
+test('login test', async()=> {
+
+    const browser:Browser = await firefox.launch({headless:false});
+    const page:Page = await browser.newPage();
+
+    await page.goto('https://www.saucedemo.com/v1/index.html');
+
+    const email:Locator = await page.locator('#user-name');
+    const pwd:Locator = await page.locator('#password');
+    const loginButton:Locator = await page.locator('#login-button');
+
+    await email.fill('standard_user');
+    await pwd.fill('secret_sauce');
+    await loginButton.click();
+
+    const pageHeader = await page.locator('.product_label').textContent();
+    console.log(`Page header is: ${pageHeader}`);
+
+     // Validate header text
+     await expect(page.locator('.product_label')).toHaveText('Products');
+
+    // Take screenshot
+     await page.screenshot({ path: 'productPage.png' });
+
+    browser.close();
+
+
+})
