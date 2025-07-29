391fbe8a06af1a93aef30811d9a802564baa4b74
Dinesh Borude <borudedinesh2@gmail.com>
2025-07-29 23:58:26 +0530

    closuresu

---
+++ sandbox_tests/JS_Learnings/closure.js
@@ -0,0 +1,18 @@
+
+function outer() {
+
+    let username = "danny2610";
+
+     function inner() {
+
+        console.log(`Inner Scopes : ${username}`);
+
+     }
+
+
+     inner();
+
+}
+
+outer();
+console.log(`OUTER SCOPE's ${username}`)
